090f2d6de63f657fa7c31de8167697c0a1399e21
Administrator <Cyuxuan>
2020-03-11 15:13:09 +0800

    HashMap自定义版本

---
+++ src/com/cyuxuan/myconllection/Node3.java
@@ -0,0 +1,23 @@
+package com.cyuxuan.myconllection;
+
+
+
+
+
+/**
+ * @author Cyuxuan
+ *
+ * @param <K>
+ * @param <V>
+ * 
+ * 类说明:
+ * 		此类是HashMap的节点类
+ */
+public class Node3<K,V> {
+	
+	int  hash;
+	K  key;
+	V  value;
+	Node3  next; 
+	
+}
